f1627c00c63e46b4456a56e52938622b7edeb79d
James Esteban <james.esteban08@gmail.com>
2025-07-31 15:05:38 +0800

    setup src path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import appLogo from '/favicon.svg'
 import PWABadge from './PWABadge.tsx'
-import './App.css'
+import '@/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
